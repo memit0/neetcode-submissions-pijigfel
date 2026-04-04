ba578e3decdcfb79ab5c84c4fe24110356ed6592
Mehmet Battal <60168662+memit0@users.noreply.github.com>
2026-04-04 08:58:37 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,19 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.length = 0
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[self.length - 1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+        
