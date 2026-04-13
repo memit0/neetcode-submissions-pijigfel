3c4b6d965df36089d9ae90850253e9de5ee93118
Mehmet Battal <60168662+memit0@users.noreply.github.com>
2026-04-13 09:17:21 -0400

    Add: mergeSort - submission-3

---
+++ Data Structures & Algorithms/mergeSort/submission-3.py
@@ -0,0 +1,54 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def mergeSort(self, pairs: List[Pair]) -> List[Pair]:
+        return self.mergeSortHelper(pairs, 0, len(pairs)-1)
+    
+    def mergeSortHelper(self, arr, s, e):
+        if (e - s) <= 0:
+            return arr
+
+        m = (s + e) // 2
+
+        self.mergeSortHelper(arr, s, m) # sort left half recursively in place
+        self.mergeSortHelper(arr, m + 1, e) # sort right half recursively in place
+
+        self.merge(arr, s, m, e)
+
+        return arr
+
+
+    def merge(self, arr, s, m, e):
+        left_arr = arr[s:m+1]
+        right_arr = arr[m+1:e+1]
+
+        i, j, k = 0, 0, s
+
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i].key <= right_arr[j].key:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+        
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+        
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+        
+
+
+        
+
+
+    
