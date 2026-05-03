9ccc19112e45393f59ec4533932f9d8513c22d38
Mehmet Battal <60168662+memit0@users.noreply.github.com>
2026-05-03 08:11:58 -0400

    Add: range-sum-query-2d-immutable - submission-1

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-1.py
@@ -0,0 +1,31 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        if not matrix or not matrix[0]:
+            self.prefix = [[0]]
+            return
+
+        n, m = len(matrix), len(matrix[0])
+        self.prefix = [[0] * m for _ in range(n)]
+
+        for r in range(n):
+            row_sum = 0
+            for c in range(m):
+                row_sum += matrix[r][c]
+                above = self.prefix[r-1][c] if r > 0 else 0
+                self.prefix[r][c] = above + row_sum
+
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        bottomRight = self.prefix[row2][col2]
+        above = self.prefix[row1 - 1][col2] if row1 > 0 else 0
+        left = self.prefix[row2][col1 - 1] if col1 > 0 else 0
+        topLeft = self.prefix[row1 - 1][col1 - 1] if row1 > 0 and col1 > 0 else 0
+    
+        return bottomRight - above - left + topLeft # since subtract by left and above the left and above both have the topleft section, so you're subtracting twice therefore you need to add once back
+        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
