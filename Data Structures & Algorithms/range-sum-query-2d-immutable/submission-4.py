1855d45d018986167b0c01d1475339862f02fbd9
Mehmet Battal <60168662+memit0@users.noreply.github.com>
2026-05-03 08:20:08 -0400

    Add: range-sum-query-2d-immutable - submission-4

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-4.py
@@ -0,0 +1,22 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        if not matrix or not matrix[0]:
+            self.prefix = [[0]]
+            return 
+
+        self.prefix = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+
+        for r in range(len(matrix)):
+            row_sum = 0
+            for c in range(len(matrix[0])):
+                row_sum += matrix[r][c]
+                above = self.prefix[r - 1][c] if r > 0 else 0
+                self.prefix[r][c] = above + row_sum
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        bottomRight = self.prefix[row2][col2]
+        top = self.prefix[row1 - 1][col2] if row1 > 0 else 0
+        left = self.prefix[row2][col1 - 1] if col1 > 0 else 0
+        topLeft = self.prefix[row1 - 1][col1 - 1] if row1 > 0 and col1 > 0 else 0
+        return bottomRight - top - left + topLeft #Top-left is added again because the left and top subtract top-left twice because that's the common intersection for both. 
