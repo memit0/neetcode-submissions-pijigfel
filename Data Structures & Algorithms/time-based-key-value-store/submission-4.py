2c610fcd2d5bf286e6e154cf8930362681d6f65b
Mehmet Battal <60168662+memit0@users.noreply.github.com>
2026-06-16 14:09:37 -0400

    Add: time-based-key-value-store - submission-4

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-4.py
@@ -0,0 +1,33 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hashmap = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.hashmap:
+            self.hashmap[key] = []
+        self.hashmap[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.hashmap:
+            return ""
+
+        search_array = self.hashmap[key]
+        l, r = 0, len(search_array) - 1
+
+        res = ""
+
+        while l <= r:
+            mid = (l + r) // 2
+            if search_array[mid][0] == timestamp:
+                return search_array[mid][1]
+            elif search_array[mid][0] <= timestamp:
+                res = search_array[mid][1]
+                l = mid + 1
+            else: 
+                r = mid - 1 
+
+
+        return res
+
+        
